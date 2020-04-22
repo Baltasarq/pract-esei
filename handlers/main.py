78e0b44ed8323c0678842cf8d15fc55d1760d4a2
baltasar <baltasarq@gmail.com>
2020-04-22 14:14:09 +0200

    Subject list index created

--- handlers/main.py
+++ handlers/main.py
@@ -17,11 +17,23 @@
 
 
 import webapp2
+from webapp2_extras import jinja2
+
+from model.asignatura import Asignatura
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        asignaturas = Asignatura.query().order(-Asignatura.curso)
+
+        valores_plantilla = {
+            "asignaturas": asignaturas
+        }
+
+        jinja = jinja2.get_jinja2(app=self.app)
+        self.response.write(
+            jinja.render_template("index.html",
+            **valores_plantilla))
 
 
 app = webapp2.WSGIApplication([
